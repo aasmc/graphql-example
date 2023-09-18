bfce097500dae7d9b761b8c59b5bb97fed3ce7ac
Alex Alexeyev <aasmc@yandex.ru>
2023-09-18 20:49:41 +0300

    Initial Commit

---
+++ client/src/main/java/ru/aasmc/client/GraphQLClientApplication.java
@@ -0,0 +1,11 @@
+package ru.aasmc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLClientApplication.class);
+    }
+}
---
+++ client/src/test/java/ru/aasmc/client/GraphQLClientApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.aasmc.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQLClientApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
